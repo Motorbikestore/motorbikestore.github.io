f37d96e126195098ff2fb8e4dec9cfbe916fb39e
Motorbikestore <mtrbikestore@gmail.com>
2021-06-18 18:02:32 -0500

    index

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,7 +1,7 @@
 class MiFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = /* html */
-            `Copyright &copy; 2021 Martinez Garcia Cristhian. (3CV50)`;
+            `Copyright &copy; 2021 Martinez Garcia Cristhian.`;
     }
 }
 customElements.define("mi-footer", MiFooter);

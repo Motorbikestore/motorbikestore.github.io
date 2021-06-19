297229c3b83ea1f8bd263b3b0ad61b1c972c7c6e
Motorbikestore <mtrbikestore@gmail.com>
2021-06-18 23:08:13 -0500

    autentificacion

---
+++ js/init.js
@@ -0,0 +1,11 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyBozilu5xeHYvT1eUz8zfUyHKJKFyqkqMk",
+    authDomain: "motorbikestore.firebaseapp.com",
+    projectId: "motorbikestore",
+    storageBucket: "motorbikestore.appspot.com",
+    messagingSenderId: "975336460898",
+    appId: "1:975336460898:web:c797b65798b00034d8f73b"
+});

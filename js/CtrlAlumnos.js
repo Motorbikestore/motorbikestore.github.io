1181e7a559fcfc84a1b6dfd5c768418ee612fff5
Motorbikestore <mtrbikestore@gmail.com>
2021-06-20 12:45:29 -0500

    precio

--- js/CtrlAlumnos.js
+++ js/CtrlAlumnos.js
@@ -68,6 +68,8 @@ function htmlFila(doc) {
   const data = doc.data();
   const marca = cod(data.marca);
   const nombre = cod(data.nombre);
+  const year = cod(data.year);
+  const precio = cond(data.precio);
   var fsf= cod(data.fecha);
   var fecha = new Date(fsf);
   var espacio="[   -   ]";
@@ -80,7 +82,7 @@ function htmlFila(doc) {
       <a class="fila" href=
   "alumno.html?${parámetros}">
         <strong class="primario">
-          ${marca} ${nombre} ${dformat}
+          ${marca} ${nombre} ${dformat} ${year} ${precio}
         </strong>
       </a>
      

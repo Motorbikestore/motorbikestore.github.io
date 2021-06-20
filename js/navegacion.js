72d0e910a88b111b3c335f49e8d1b4012574809e
Motorbikestore <mtrbikestore@gmail.com>
2021-06-20 14:36:11 -0500

    corchetes

--- js/navegacion.js
+++ js/navegacion.js
@@ -8,7 +8,8 @@ export function
   location.href =
     "motocicletas.html";
 }
-export function muestramotocicletasCliente {
+
+export function muestramotocicletasCliente() {
   location:href = 
   "motocicletasCliente.html"
 }

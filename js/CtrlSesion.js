ff0cfc5087a363bf50f2082746413736d06bfc87
Motorbikestore <mtrbikestore@gmail.com>
2021-06-20 05:09:23 -0500

    ctrl

--- js/CtrlSesion.js
+++ js/CtrlSesion.js
@@ -49,7 +49,7 @@ async function
       usuario.photoURL || "";
     forma.terminarSesión.
       addEventListener(
-        "click", terminaSesión);
+        "click", terminarSesión);
   } else {
     // No ha iniciado sesión.
     iniciaSesión();
